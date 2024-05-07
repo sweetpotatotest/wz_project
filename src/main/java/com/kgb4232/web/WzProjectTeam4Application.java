5da5cc2bbdcf38af8a0da955264e7b33d55f8dcd
sweetpotatotest <kgb4232@daum.net>
2024-05-07 14:33:34 +0900

    test

---
+++ src/main/java/com/kgb4232/web/WzProjectTeam4Application.java
@@ -0,0 +1,13 @@
+package com.kgb4232.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzProjectTeam4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WzProjectTeam4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kgb4232/web/WzProjectTeam4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgb4232.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzProjectTeam4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
